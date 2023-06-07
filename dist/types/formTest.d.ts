a71e4a0861522a278bd287fe62595ca60f2b6020
donglei <405267330@qq.com>
2023-06-07 14:24:02 +0800

    打包

---
+++ dist/types/formTest.d.ts
@@ -0,0 +1,54 @@
+/**
+ * 校验是否是手机号格式
+ * @param tel - 字符串
+ * @returns 是返回`true`,否返回`false`
+ */
+export declare function ValidatorTel(tel: string): boolean;
+/**
+ * 校验是否为电子邮箱格式
+ * @param email - 字符串
+ * @returns 是返回`true`,否返回`false`
+ */
+export declare function ValidatorEmail(email: string): boolean;
+/**
+ * 校验是否为身份证号格式
+ * @param idCard - 字符串
+ * @returns 是返回`true`,否返回`false`
+ */
+export declare function ValidatorIdCard(idCard: string): boolean;
+/**
+ * 校验是否为银行卡号格式
+ * @param bankCard - 字符串
+ * @returns 是返回`true`,否返回`false`
+ */
+export declare function ValidatorBankCard(bankCard: string): boolean;
+/**
+ * 校验是否为汉字
+ * @param chinese - 字符串
+ * @returns 是返回`true`,否返回`false`
+ */
+export declare function ValidatorChinese(chinese: string): boolean;
+/**
+ * 校验是否为字母
+ * @param letter - 字符串
+ * @returns 是返回`true`,否返回`false`
+ */
+export declare function ValidatorLetter(letter: string): boolean;
+/**
+ * 校验是否为url域名
+ * @param url - 字符串
+ * @returns 是返回`true`,否返回`false`
+ */
+export declare function ValidatorUrl(url: string): boolean;
+/**
+ * 校验是否为1~50位的汉字数字字母
+ * @param cca - 字符串
+ * @returns 是返回`true`,否返回`false`
+ */
+export declare function ValidatorCCA(cca: string): boolean;
+/**
+ * 校验是否为分数
+ * @param score - 字符串
+ * @returns 是返回`true`,否返回`false`
+ */
+export declare function ValidatorScore(score: string): boolean;
---
+++ dist/types/fscHelper.d.ts
@@ -0,0 +1,25 @@
+/**
+ * 浏览器全屏
+ * @param element dom元素选择器
+ */
+export declare function fullscreen(element: Element): void;
+/**
+ * 退出浏览器全屏模式
+ */
+export declare function exitFullscreen(): void;
+/**
+ * 判读是否支持浏览器全屏
+ * @returns 支持返回true,否则返回false
+ */
+export declare function fullscreenEnabled(): boolean;
+/**
+ * 判断浏览器是否全屏
+ * @returns 全屏则返回当前调用全屏的元素,不全屏返回false
+ */
+export declare function isFullscreen(): boolean | Element;
+/**
+ * 添加/移除 浏览器全屏事件监听
+ * @param type 类型 添加事件'addEventListener', 移除事件'removeEventListener'
+ * @param fullscreenchange 全屏模式变化后的回调函数
+ */
+export declare function fullScreenListener(type: "addEventListener" | "removeEventListener", fullscreenchange: (e: any) => void): void;
