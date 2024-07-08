74dd9557e4258dc1903016f71bfe56d9e243cc12
donglei <donglei@qq.com>
2024-07-08 16:02:49 +0800

    fix: 补充getFileExtension方法;previewPdf支持传参控制工具栏显示隐藏

--- src/file.ts
+++ src/file.ts
@@ -10,34 +10,34 @@
  * ```
  */
 export function tagADownload(options: { url: string; title: string; target?: string }) {
-    const { url = "", title = "", target = "_blank" } = options
-    const tagA = document.createElement("a")
-    tagA.setAttribute("href", url)
-    tagA.setAttribute("download", title)
-    tagA.setAttribute("target", target)
-    document.body.appendChild(tagA)
-    tagA.click()
-    document.body.removeChild(tagA)
+  const { url = '', title = '', target = '_blank' } = options
+  const tagA = document.createElement('a')
+  tagA.setAttribute('href', url)
+  tagA.setAttribute('download', title)
+  tagA.setAttribute('target', target)
+  document.body.appendChild(tagA)
+  tagA.click()
+  document.body.removeChild(tagA)
 }
 
 // 文件类型
 enum FileType {
-    excel = "application/vnd.ms-excel",
-    xls = "application/vnd.ms-excel",
-    xlsx = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    pdf = "application/pdf",
-    doc = "application/msword",
-    docx = "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    zip = "application/zip",
-    rar = "application/rar",
-    png = "image/png",
-    webp = "image/webp",
-    bmp = "image/bmp",
-    jpeg = "image/jpeg",
-    gif = "image/gif",
-    tiff = "image/tiff",
-    svg = "image/svg+xml",
-    mp4 = "video/mp4",
+  excel = 'application/vnd.ms-excel',
+  xls = 'application/vnd.ms-excel',
+  xlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  pdf = 'application/pdf',
+  doc = 'application/msword',
+  docx = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  zip = 'application/zip',
+  rar = 'application/rar',
+  png = 'image/png',
+  webp = 'image/webp',
+  bmp = 'image/bmp',
+  jpeg = 'image/jpeg',
+  gif = 'image/gif',
+  tiff = 'image/tiff',
+  svg = 'image/svg+xml',
+  mp4 = 'video/mp4',
 }
 
 type FILETYPE = keyof typeof FileType
@@ -53,52 +53,53 @@ type FILETYPE = keyof typeof FileType
  * ```
  */
 export const downloadBlobFile = (res: Blob, name: string, type?: FILETYPE) => {
-    const blob = type ? new Blob([res], { type: FileType[type] }) : new Blob([res])
-    if ("download" in document.createElement("a")) {
-        const tagA = document.createElement("a")
-        tagA.download = name
-        tagA.style.display = "none"
-        tagA.href = URL.createObjectURL(blob)
-        document.body.appendChild(tagA)
-        tagA.click()
-        URL.revokeObjectURL(tagA.href)
-        document.body.removeChild(tagA)
-    }
+  const blob = type ? new Blob([res], { type: FileType[type] }) : new Blob([res])
+  if ('download' in document.createElement('a')) {
+    const tagA = document.createElement('a')
+    tagA.download = name
+    tagA.style.display = 'none'
+    tagA.href = URL.createObjectURL(blob)
+    document.body.appendChild(tagA)
+    tagA.click()
+    URL.revokeObjectURL(tagA.href)
+    document.body.removeChild(tagA)
+  }
 }
 
 /**
  * 预览PDF文件
  * @param res - pdf文件流
+ * @param hiddenToolBar - 是否隐藏工具栏
  */
-export const previewPdf = (res: Blob) => {
-    const blob = new Blob([res], { type: "application/pdf" })
-    if ("download" in document.createElement("a")) {
-        const tagA = document.createElement("a")
-        tagA.style.display = "none"
-        tagA.href = `${URL.createObjectURL(blob)}#scrollbars=0&toolbar=0&statusbar=0`
-        tagA.target = "_blank"
-        document.body.appendChild(tagA)
-        tagA.click()
-        URL.revokeObjectURL(tagA.href)
-        document.body.removeChild(tagA)
-    }
+export const previewPdf = (res: Blob, hiddenToolBar = true) => {
+  const blob = new Blob([res], { type: 'application/pdf' })
+  if ('download' in document.createElement('a')) {
+    const tagA = document.createElement('a')
+    tagA.style.display = 'none'
+    tagA.href = `${URL.createObjectURL(blob)}${hiddenToolBar ? '#scrollbars=0&toolbar=0&statusbar=0' : ''}`
+    tagA.target = '_blank'
+    document.body.appendChild(tagA)
+    tagA.click()
+    URL.revokeObjectURL(tagA.href)
+    document.body.removeChild(tagA)
+  }
 }
 
 /**
  * 预览图片
- * @param res - 图片文件流
+ * @param file - 图片文件流
  */
 export const previewImage = (file: Blob) => {
-    if ("download" in document.createElement("a")) {
-        const tagA = document.createElement("a")
-        tagA.style.display = "none"
-        tagA.href = URL.createObjectURL(file)
-        tagA.target = "_blank"
-        document.body.appendChild(tagA)
-        tagA.click()
-        URL.revokeObjectURL(tagA.href)
-        document.body.removeChild(tagA)
-    }
+  if ('download' in document.createElement('a')) {
+    const tagA = document.createElement('a')
+    tagA.style.display = 'none'
+    tagA.href = URL.createObjectURL(file)
+    tagA.target = '_blank'
+    document.body.appendChild(tagA)
+    tagA.click()
+    URL.revokeObjectURL(tagA.href)
+    document.body.removeChild(tagA)
+  }
 }
 
 /**
@@ -111,12 +112,22 @@ export const previewImage = (file: Blob) => {
  * ```
  */
 export const convertSize = (fileSize: string | null) => {
-    if (fileSize === null || fileSize === "") return "0Kb"
-    const unitArr = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
-    let index = 0
-    const originSize = parseFloat(fileSize)
-    index = Math.floor(Math.log(originSize) / Math.log(1024))
-    const size = originSize / Math.pow(1024, index)
-    const sizeString = size.toFixed(2)
-    return sizeString + unitArr[index]
+  if (fileSize === null || fileSize === '') return '0Kb'
+  const unitArr = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  let index = 0
+  const originSize = parseFloat(fileSize)
+  index = Math.floor(Math.log(originSize) / Math.log(1024))
+  const size = originSize / Math.pow(1024, index)
+  const sizeString = size.toFixed(2)
+  return sizeString + unitArr[index]
+}
+
+/**
+ * 根据文件名获取文件扩展名
+ * @param fileName 文件名称
+ * @returns string
+ */
+export const getFileExtension = (fileName: string): string => {
+  const nameIndex: number = fileName.lastIndexOf('.')
+  return fileName.substring(nameIndex + 1)?.toLowerCase() || ''
 }
