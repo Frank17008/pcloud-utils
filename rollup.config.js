b92297eba95d42acf3fe689f0dbae7e749bc0bd8
donglei <40267330@qq.com>
2025-05-30 10:53:20 +0800

    fix: 升级rollup插件

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,31 @@
-import del from "rollup-plugin-delete"
-import resolve from "@rollup/plugin-node-resolve"
-import commonjs from "@rollup/plugin-commonjs"
-import typescript from "@rollup/plugin-typescript"
-import cleanup from "rollup-plugin-cleanup"
-import { terser } from "rollup-plugin-terser"
-import json from "rollup-plugin-json"
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+import json from "@rollup/plugin-json";
+import del from "rollup-plugin-delete";
+import cleanup from "rollup-plugin-cleanup";
 
 export default {
-    input: "./src/index.ts",
-    output: [
-        {
-            name: "PUtils",
-            file: "dist/index.umd.js",
-            format: "umd",
-        },
-        {
-            file: "dist/index.esm.js",
-            format: "esm",
-        },
-    ],
-    plugins: [del({ targets: "dist/*", verbose: true }), json(), resolve(), commonjs(), typescript(), terser(), cleanup()],
-}
+  input: "./src/index.ts",
+  output: [
+    {
+      name: "PUtils",
+      file: "dist/index.umd.js",
+      format: "umd",
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+    },
+  ],
+  plugins: [
+    del({ targets: "dist/*", verbose: true }),
+    json(),
+    resolve(),
+    commonjs(),
+    typescript(),
+    terser(),
+    cleanup(),
+  ],
+};
