e406d1f92fafdd6d7b540a462d3c49049b4bfe73
donglei <40267330@qq.com>
2025-05-30 10:54:34 +0800

    feat: add getRandomNumber function and enhance treeHelper with flatToList and flatToTree methods
    - Implemented getRandomNumber function to generate random numbers within a specified range with optional decimal precision.
    - Enhanced treeHelper with flatToList and flatToTree functions for converting between tree and flat structures.
    - Updated type definitions for numHelper and treeHelper to include new functionalities and examples.

--- dist/types/numHelper.d.ts
+++ dist/types/numHelper.d.ts
@@ -20,3 +20,22 @@ export declare function convertToChinese(num: string): string;
  * ```
  */
 export declare function thousand(num: string | number): string;
+interface GetRandomNumberOptions {
+    toFixed?: number;
+}
+/**
+ * 生成指定范围内的随机数
+ *
+ * @param max 随机数的上限（不包含）
+ * @param min 随机数的下限（包含）
+ * @param options 可选配置项，包括小数点后保留的位数
+ * @param options.toFixed 小数点后保留的位数，默认为0
+ * @returns 在指定范围内的随机数，根据options配置四舍五入到指定的小数位
+ * @example
+ * ```ts
+ * import { numHelper } from "@pointcloud/pcloud-utils"
+ * numHelper.getRandomNumber(1, 10, { toFixed: 2 }) // 生成1到10之间的随机数，保留两位小数
+ * ```
+ */
+export declare function getRandomNumber(min: number, max: number, options?: GetRandomNumberOptions): number;
+export {};
--- dist/types/treeHelper.d.ts
+++ dist/types/treeHelper.d.ts
@@ -11,6 +11,10 @@ export interface TreeNode {
  * @param node 源数组
  * @param fn   过滤函数
  * @returns 查找到的目标节点
+ * @example
+ * const tree = [{ id: '1', children: [{ id: '2', children: [] }] }];
+ * const result = deepFind(tree, node => node.id === '2');
+ * // 返回 { id: '2', children: [] }
  */
 export declare function deepFind(node: TreeNode[], fn: Function): TreeNode | undefined;
 /**
@@ -19,6 +23,10 @@ export declare function deepFind(node: TreeNode[], fn: Function): TreeNode | und
  * @param fn   过滤函数
  * @param result 存放查找的结果数组
  * @returns 查找到的目标节点
+ * @example
+ * const tree = [{ id: '1', children: [{ id: '2', children: [] }] }];
+ * const result = deepFindPath(tree, node => node.id === '2');
+ * // 返回 [ { id: '1', ... }, { id: '2', ... } ]
  */
 export declare function deepFindPath(node: TreeNode[], fn: Function, result?: any[]): TreeNode[] | undefined;
 /**
@@ -27,6 +35,10 @@ export declare function deepFindPath(node: TreeNode[], fn: Function, result?: an
  * @param fn   过滤函数
  * @param result 存放查找的结果数组
  * @returns 查找到的目标节点
+ * @example
+ * const tree = [{ id: '1', children: [{ id: '2', children: [] }] }];
+ * const result = findFirstNode(tree, node => node.id === '2');
+ * // 返回 { id: '2', children: [] }
  */
 export declare function findFirstNode(node: TreeNode[], fn: Function, result?: any[]): TreeNode;
 /**
@@ -61,3 +73,57 @@ export declare function getTreeLeaf(treeData: TreeNode[] | TreeNode, leafList?: 
 export declare const getParentIdsByTreeId: (treeData: TreeNode[], nodeId: string | number, config?: {
     [key: string]: string;
 } | undefined) => string[] | number[];
+/**
+ * 将树形结构数据转换为平级结构数组
+ * @param treeData 树形结构数据
+ * @param result   用于存储扁平化结果的数组
+ * @returns 返回平级结构数组
+ * @example
+ * const tree = [
+ *   {
+ *     id: '1',
+ *     name: 'Node1',
+ *     children: [
+ *       { id: '2', name: 'Node2', children: [ { id: '2-1', name: 'Node2-1' } ] },
+ *       { id: '3', name: 'Node3'}
+ *     ]
+ *   }
+ * ];
+ * const list = flatToList(tree);
+ * // 返回 [{id: '1', name: 'Node1', parentId: undefined}, {id: '2', name: 'Node2', parentId: '1'}, {id: '2-1', name: 'Node2-1', parentId: '2'},{id: '3', name: 'Node3', parentId: '1'}]
+ */
+export declare function flatToList(treeData: TreeNode[] | TreeNode, result?: Omit<TreeNode, "children">[], parentId?: string | number): Omit<TreeNode, "children">[];
+/**
+ * 将平级结构数组转换为树形结构
+ * @param flatList 平级结构数组
+ * @param options 配置选项
+ * @param options.idKey 节点唯一标识的键名，默认为 "id"
+ * @param options.parentKey 父节点标识的键名，默认为 "parentId"
+ * @param options.childrenKey 子节点数组的键名，默认为 "children"
+ * @returns 返回树形结构数据
+ * @example
+ * const list = [
+ *   { id: '1', name: 'Node1', parentId: '0' },
+ *   { id: '2', name: 'Node2', parentId: '1' },
+ *   { id: '2-1', name: 'Node2-1', parentId: '2' },
+ *   { id: '3', name: 'Node3', parentId: '1' }
+ * ];
+ * const tree = convertFlatToTree(list);
+ * // 返回树形结构:
+ * // [
+ * //   {
+ * //     id: '1',
+ * //     name: 'Node1',
+ * //     parentId: '0',
+ * //     children: [
+ * //       { id: '2', name: 'Node2', parentId: '1', children: [ { id: '2-1', name: 'Node2-1', parentId: '2', children: [] }] },
+ * //       { id: '3', name: 'Node3', parentId: '1', children: [] }
+ * //     ]
+ * //   }
+ * // ]
+ */
+export declare function flatToTree(flatList: TreeNode[], options?: {
+    idKey: string;
+    parentKey: string;
+    childrenKey: string;
+}): TreeNode[];
